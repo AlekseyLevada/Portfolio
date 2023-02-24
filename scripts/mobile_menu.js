d05b680f1e9474aba3895defd9d02c317fd007ce
Aleksey <aleksey.levada@gmail.com>
2023-02-24 20:22:54 +0300

    'contacts-update'

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,18 +1,26 @@
 var burgerMenu = $('.burger-menu')
 var mobileMenu = $('.mobile-menu')
+var mobileMenulink = $('.mobile-menu a')
 var headerClose = $('.mobile-menu__header-close')
 var body = $('body')
 
-burgerMenu.click(function(){
+burgerMenu.click( () => {
     mobileMenu.css({
         transition:'.2s',
         transform:'translateX(0%)',
     })
 })
 
-headerClose.click(function(){
+headerClose.click( () => {
     mobileMenu.css({
         transition:'.2s',
         transform:'translateX(105%)',
     })
+})
+
+mobileMenulink.click( () => {
+    setTimeout(()=> {
+        headerClose.click()  
+    },)
+    
 })
