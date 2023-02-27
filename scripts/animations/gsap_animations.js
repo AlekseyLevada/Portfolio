8538e4c898356c2974d8b45b7205154b780a5be6
Aleksey <aleksey.levada@gmail.com>
2023-02-27 18:12:41 +0300

    'animated_first_screen'

---
+++ scripts/animations/gsap_animations.js
@@ -0,0 +1,12 @@
+gsap.from('.call__button', {duration: 3, opacity: 0})
+
+gsap.from('.container__sidebar', {duration: 1, delay: .5, opacity: 0, x: -300})
+gsap.from('.intro__main_content', {duration: 1, delay: .5, opacity: 0})
+
+gsap.from('.sidebar__logo', { duration: .7, delay: 1.5, x: -200, opacity: 0})
+gsap.from('.container__cascade', { duration: 1, delay: 2, x: -200, opacity: 0})
+gsap.from('.socials', { duration: .7, delay: 2.7, x: -200, opacity: 0})
+
+gsap.from('.intro__navbar', {duration: 0.7,delay: 1.5, x: 600, opacity: 0})
+
+gsap.from('.container__main-text', {duration: 2, delay: 3, opacity: 0})
