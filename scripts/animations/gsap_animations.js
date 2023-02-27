621cfeb482c3fde2000126d6d74b45e492ef5977
Aleksey <aleksey.levada@gmail.com>
2023-02-27 22:11:42 +0300

    'intro_gsap_anim'

--- scripts/animations/gsap_animations.js
+++ scripts/animations/gsap_animations.js
@@ -1,12 +1,62 @@
-gsap.from('.call__button', {duration: 3, opacity: 0})
+gsap.registerPlugin(ScrollTrigger)
 
-gsap.from('.container__sidebar', {duration: 1, delay: .5, opacity: 0, x: -300})
-gsap.from('.intro__main_content', {duration: 1, delay: .5, opacity: 0})
+gsap.from('.call__button', {duration: 3, delay: 3, opacity: 0})
 
-gsap.from('.sidebar__logo', { duration: .7, delay: 1.5, x: -200, opacity: 0})
-gsap.from('.container__cascade', { duration: 1, delay: 2, x: -200, opacity: 0})
-gsap.from('.socials', { duration: .7, delay: 2.7, x: -200, opacity: 0})
+const tlInnerSidebar = gsap.timeline()
+tlInnerSidebar.from('.container__sidebar', {
+    duration: 2,
+    delay:.2,
+    opacity: 0,
+})
+.from('.sidebar__logo', {
+    duration: .5,
+    x: -200,
+    opacity: 0,
+})
+.from('.container__cascade', {
+    duration: 1,
+    x: -200,
+    opacity: 0,
+})
+.from('.socials', {
+    duration: .7,
+    opacity: 0,
+    x: -200,
+    ease: "bounce.out",
+})
 
-gsap.from('.intro__navbar', {duration: 0.7,delay: 1.5, x: 600, opacity: 0})
+const tlInnerIntro = gsap.timeline()
+tlInnerIntro.from('.footer-line', {
+    duration: 2,
+    delay:.2,
+    opacity: 0,
+})
+.from('.intro__navbar', {
+    duration: 1,
+    y: -50,
+})
+.from('.container__main-text', {
+    duration: 1,
+    yPercent: 50,
+    opacity: 0,
+})
 
-gsap.from('.container__main-text', {duration: 2, delay: 3, opacity: 0})
+
+gsap.to('.container__cascade', {
+    scrollTrigger: {
+        trigger: '.intro',
+        start: '10% top',
+        scrub: true,
+    },
+    y: 100,
+    scale: .8,
+})
+gsap.to('.container__main-text', {
+    scrollTrigger: {
+        trigger: '.intro',
+        start: '10% top',
+        scrub: true,
+    },
+    y: 200,
+    scale: .8,
+})
